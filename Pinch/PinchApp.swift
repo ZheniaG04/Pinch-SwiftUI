49fa6932ad6d4d4d156176bdb187a08d1fb2fa82
Yevhenii Huts <yevhenii.huts@globallogic.com>
2022-05-22 21:49:01 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Yevhenii Huts on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
